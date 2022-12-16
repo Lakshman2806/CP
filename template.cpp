4daec761d6c1afb63a97caa75237db2f8109a091
Lakshman2806 <96191782+Lakshman2806@users.noreply.github.com>
2022-12-16 20:15:24 +0530

    create template

---
+++ template.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int lli;
+
+lli lmax(lli a, lli b)
+{
+    return a > b ? a : b;
+}
+
+lli lmin(lli a, lli b)
+{
+    return a > b ? b : a;
+}
+
+lli power(long long x, lli y, long long int p)
+{
+    lli res = 1; // Initialize result
+
+    x = x % p; // Update x i} it is more than or
+               // equal to p
+
+    if (x == 0)
+        return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+#define ar array
+#define vl vector<lli>
+#define pl pair<lli, lli>
+#define for_(i, x, n) for (lli i = x; i < n; i++)
+#define endl "\n"
+
+void solve()
+{
+    lli n;
+    cin >> n;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    lli tc;
+    cin >> tc;
+
+    for_(t, 1, tc + 1)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
