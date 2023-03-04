6501d123fef086b68b34f620c4c942257c120319
Lakshman2806 <96191782+Lakshman2806@users.noreply.github.com>
2023-03-04 11:12:03 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -38,15 +38,32 @@ lli power(long long x, lli y, long long int p)
 }
 #define ar array
 #define vi vector<lli>
+#define usi unordered_set<lli>
+#define si set<lli>
+
 #define pi pair<lli, lli>
 #define for_(i, x, n) for (lli i = x; i < n; i++)
 #define endl "\n"
+#define mod 1000000007
+#define umi unordered_map<lli, lli>
+// vi ans(100000, 0);
+const int maxn = 100005;
+int a[maxn];
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
 
 void solve(lli t, lli tc)
 {
-    lli n;
-    cin >> n;
 }
+
+
+void iniatialization()
+{
+}
+
+// cout << ans << endl;
 
 int main(void)
 {
@@ -56,7 +73,9 @@ int main(void)
 
     lli tc;
     cin >> tc;
-
+    // tc = 1;
+    // ans[1] = 1;
+    iniatialization();
     for_(t, 1, tc + 1)
     {
         // cout << "Case #" << t << ": ";
