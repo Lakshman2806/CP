790d24878a70dc63d5a3e4150b971180cff7de05
Lakshman2806 <96191782+Lakshman2806@users.noreply.github.com>
2023-07-14 22:20:32 +0530

    Added prime number generator

--- template.cpp
+++ template.cpp
@@ -14,7 +14,7 @@ lli lmin(lli a, lli b)
     return a > b ? b : a;
 }
 
-lli power(long long x, lli y, long long int p)
+lli lpower(long long x, lli y, long long int p)
 {
     lli res = 1; // Initialize result
 
@@ -47,17 +47,58 @@ lli power(long long x, lli y, long long int p)
 #define mod 1000000007
 #define umi unordered_map<lli, lli>
 // vi ans(100000, 0);
-const int maxn = 100005;
-int a[maxn];
+
 int gcd(int a, int b)
 {
     return b == 0 ? a : gcd(b, a % b);
 }
 
-void solve(lli t, lli tc)
+void SieveOfEratosthenes(lli n, set<lli> &s)
 {
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (lli p = 2; p * p <= n; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p greater than or
+            // equal to the square of it numbers which are
+            // multiple of p and are less than p^2 are
+            // already been marked.
+            for (lli i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // Print all prime numbers
+    for (lli p = 2; p <= n; p++)
+        if (prime[p])
+            s.insert(p);
+}
+lli Kadane(vector<lli> &A)
+{
+    lli max_so_far = INT_MIN, max_ending_here = 0;
+    for (lli i = 0; i < A.size(); i++)
+    {
+        max_ending_here = max_ending_here + A[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
 }
 
+void solve(lli t, lli tc)
+{
+    
+}
 
 void initialization()
 {
@@ -74,7 +115,6 @@ int main(void)
     lli tc;
     cin >> tc;
     // tc = 1;
-    // ans[1] = 1;
     initialization();
     for_(t, 1, tc + 1)
     {
