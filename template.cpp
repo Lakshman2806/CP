1af749c64e797c5e0f2581973bf35826e210137b
Lakshman <96191782+Lakshman2806@users.noreply.github.com>
2023-08-12 09:25:37 +0530

    Added LIS

--- template.cpp
+++ template.cpp
@@ -95,6 +95,26 @@ lli Kadane(vector<lli> &A)
     return max_so_far;
 }
 
+lli lis(vector<int> &v)
+{
+    int n = v.size();
+    vector<int> ans;
+    ans.push_back(v[0]);
+    for(int i = 1; i < n; i++)
+    {
+        if(v[i] > ans.back())
+        {
+            ans.push_back(v[i]);
+        }
+        else
+        {
+            int idx = lower_bound(ans.begin(), ans.end(), v[i]) - ans.begin();
+            ans[idx] = v[i];
+        }
+    }
+    return ans.size();
+}
+
 void solve(lli t, lli tc)
 {
     
